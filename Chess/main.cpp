b297c7b407518c577a5e8d580f0eed1d6cc5d296
IsaacPrice <iprice25250@gmail.com>
2023-06-01 12:40:50 -0400

    The new version of chess is now functional as a board, more features to add later

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -13,10 +13,35 @@ struct pos {
     int y;
 };
 
-void drawBoard(vector<vector<string>> Board) {
+class chess {
+public:
+    chess();
+    chess(vector<vector<string>> Board);
+
+    void drawBoard();
+    bool movePiece(pos piece, pos location);
+    bool verify(string toVerify);
+
+private:
+    vector<vector<string>> board;
+
+};
+
+// Default constuctor for the chess class
+chess::chess() {
+    cout << "Welcome to chess!" << endl;
+}
+
+// Constuctor with the board already
+chess::chess(vector<vector<string>> Board) {
+    board = Board;
+}
+
+void chess::drawBoard() {
     cout << "  a b c d e f g h" << endl;
     int number = 0;
-    for (auto& xVect : Board) {
+    
+    for (auto& xVect : board) {
         number++;
         cout << number << " ";
         for (auto& piece : xVect) {
@@ -65,22 +90,31 @@ void drawBoard(vector<vector<string>> Board) {
 }
 
 // Moves the piece to the desired location
-bool movePiece(vector<vector<string>> Board, pos piece, pos location) {
+bool chess::movePiece(pos piece, pos location) {
     string pieceName;
 
-    // Verifying that all of the positions are legal
-    if (!(piece.x < 9 && piece.y < 9 && location.x < 9 && location.y < 9)) {
-        cout << "Invalid Move." << endl;
-        return false;
-    }
+    pieceName = board.at(piece.y - 1).at(piece.x - 1);
+    board.at(piece.y - 1).at(piece.x - 1) = "";
 
-    pieceName = Board.at(piece.y - 1).at(piece.x - 1);
-
-    Board.at(location.y - 1).at(location.x - 1) = pieceName;
+    board.at(location.y - 1).at(location.x - 1) = pieceName;
 }
 
+// This makes sure that the string is valid
+bool chess::verify(string toVerify) {
+    if (int(toupper(toVerify[0])) - 64 < 9 && int(toVerify[1]) - 48 < 9) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// This gets the users input and formats it to a position
 pos decipher(string something) {
     pos toSend;
+    toSend.y = int(something[1]) - 48;
+    toSend.x = int(toupper(something[0])) - 64;
+    return toSend;
 }
 
 int main() {
@@ -97,11 +131,32 @@ int main() {
         {"wpawn","wpawn","wpawn","wpawn","wpawn","wpawn","wpawn","wpawn"},
         {"wrook","wknight","wbishop","wking","wqueen","wbishop","wknight","wrook"}
     };
+    chess game(board);
 
+    // This will get all of the movements of the pieces
     while (isGoing) {
-        drawBoard(board);
+        game.drawBoard();
+
+        // This makes sure that the user enters a valid string
         cout << "Enter the piece you would like to move: ";
-        cin >> userInput;
+        cin >> userInput;  
+        while (!game.verify(userInput)) {
+            cout << "Entered an Invalid place" << endl;
+            cout << "Enter the piece you would like to move: ";
+            cin >> userInput;  
+        } 
+        selected = decipher(userInput);
+
+        cout << "Enter the place you would like to move: ";
+        cin >> userInput;  
+        while (!game.verify(userInput)) {
+            cout << "Entered an Invalid place" << endl;
+            cout << "Enter the place you would like to move: ";
+            cin >> userInput;  
+        } 
+        destination = decipher(userInput);
+
+        game.movePiece(selected, destination);
     }
 
     return 0;
