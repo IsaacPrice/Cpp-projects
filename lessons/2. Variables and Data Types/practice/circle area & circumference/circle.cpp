83e1d1dd9915ea97bf85a032d139217284de8c06
IsaacPrice <iprice25250@gmail.com>
2023-06-16 06:42:41 -0600

    Add in documentation

---
+++ lessons/2. Variables and Data Types/practice/circle area & circumference/circle.cpp
@@ -0,0 +1,5 @@
+// This program will get the radius of the circle from the user, and calculate the area and circumference, 
+// then print it onto the screen
+
+// area = PI * radius * radius
+// perimeter = PI * 2 * radius
--- lessons/2. Variables and Data Types/practice/circle area & circumference/circleKey.cpp
+++ lessons/2. Variables and Data Types/practice/circle area & circumference/circleKey.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 using namespace std;
 
+// This creates a global variable that is the value of PI
 double PI = 3.14159265358979323846;
 
 int main() {
@@ -13,6 +14,12 @@ int main() {
     cin >> radius;
 
     // Calculates the area and circumference
+    double area = PI * radius * radius;
+    double circumference = 2 * PI * radius;
+
+    // Prints the results onto the screen
+    cout << "Area: " << area << endl;
+    cout << "Circumference: " << circumference << endl;
 
     return 0;
 }
