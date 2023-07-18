8c2f7004f0a108db1a4c0b5b1823afc04ee8fdb0
IsaacPrice <iprice25250@gmail.com>
2023-07-18 17:47:15 -0400

    Added in a new Leetcode problem

---
+++ leetCode problems/4. Median of Two Sorted Arrays/first.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> combined = nums1;
+        for (auto& num : nums2) {
+            combined.push_back(num);
+        }
+
+        // Sort them
+        sort(combined.begin(), combined.end());
+
+        // They are calculated diffrently if it is even or odd
+        if (combined.size() % 2 == 0) {
+            // Gets the average of the two need numbers
+            double num = combined.at(combined.size() / 2) + combined.at(combined.size() / 2 - 1);
+            return num / 2.0;
+        }
+        else {
+            return combined.at(int(combined.size() / 2.0));
+        }
+        return 0;
+
+    }
+};
